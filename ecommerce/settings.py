792a318ceec234d62656c6271b2907c5d548d9b7
Shameer Eranhikath <shmr_ekth@yahoo.com>
2020-09-21 20:45:02 +0400

    Temporary templates are added

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -26,7 +26,6 @@ SECRET_KEY = '^30z7sji+to6vdvqcro_oz86b-^x61h6t7inu6*d^bn^w+abl8'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -80,7 +78,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -100,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -114,8 +110,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
--- ecommercestore/views.py
+++ ecommercestore/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+# view for Store
+def store(request):
+    context = {}
+    return render(request, 'store.html', context)
+
+
+# View for Cart
+def cart(request):
+    context = {}
+    return render(request, 'cart.html', context)
+
+
+# View  for Checkout
+def checkout(request):
+    context = {}
+    return render(request, 'checkout.html', context)
